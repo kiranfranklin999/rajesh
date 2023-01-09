a00ce3d6ee624e6c3d749357020e48cf74dd81a9
kf154363 <kiran.x.franklin@gsk.com>
2023-01-09 16:05:40 +0530

    new codes

--- newArchi/hyperparamter.py
+++ newArchi/hyperparamter.py
@@ -8,15 +8,23 @@ def build_model(hp):
     hp_activation_1 = hp.Choice('activation_1', values=['relu', 'tanh'])
     hp_activation_2 = hp.Choice('activation_2', values=['relu', 'tanh'])
     hp_activation_3 = hp.Choice('activation_3', values=['relu', 'tanh'])
-    kwargs = {'hp_activation_0':hp_activation_1,'hp_activation_1':hp_activation_2,'hp_activation_2':hp_activation_3}
+    hp_activation_4 = hp.Choice('activation_4', values=['relu', 'tanh'])
+    hp_activation_5 = hp.Choice('activation_5', values=['relu', 'tanh'])
+    hp_activation_6 = hp.Choice('activation_6', values=['relu', 'tanh'])
+    hp_activation_7 = hp.Choice('activation_7', values=['relu', 'tanh'])
+    hp_activation_8 = hp.Choice('activation_8', values=['relu', 'tanh'])
+    kwargs = {'hp_activation_1':hp_activation_1,'hp_activation_2':hp_activation_2,'hp_activation_3':hp_activation_3,'hp_activation_4':hp_activation_4,
+            'hp_activation_5':hp_activation_5,'hp_activation_6':hp_activation_6,'hp_activation_7':hp_activation_7,'hp_activation_8':hp_activation_8}
     hp_weights=hp.Choice('weights', values=['glorot_uniform','he_normal','he_uniform','random_uniform','random_normal'])
-    for i,r in enumerate(range(hp.Int('num_layers', 2, 4))):
+    model.add(layers.Input(16))
+    
+    for i,r in enumerate(range(hp.Int('num_layers', 2, 8))):
         print(i,r)
-        model.add(layers.Dense(units=hp.Int('units_' + str(i),
+        model.add(layers.Dense(units=hp.Int('units_' + str(i+1),
                                             min_value=2,
                                             max_value=10,
                                             step=2),kernel_initializer=hp_weights,
-                               activation=kwargs[f'hp_activation_{i}']))
+                               activation=kwargs[f'hp_activation_{i+1}']))
     model.add(layers.Dense(1, activation='linear'))
     model.compile(
         optimizer=keras.optimizers.Adam(
@@ -33,7 +41,7 @@ def tune_model(train_x,train_y,val_x,val_y,epochs: int,batch_size: int,project_n
              validation_data=(val_x, val_y))
     best_params = tuner.get_best_hyperparameters()
     best_model = tuner.get_best_models()[0]
-    return best_params,best_model
+    return best_params
 
 if __name__=="__main__":
     dep_train=pd.read_csv("D:\\bro\\Analysis\\mSANN\\cd\\XTr.dat",sep='\s+',names=[str(i) for i in range(0,16)])
@@ -43,7 +51,7 @@ if __name__=="__main__":
     dep_test=pd.read_csv("D:\\bro\\Analysis\\mSANN\\cd\\XT.dat",sep='\s+',names=[str(i) for i in range(0,16)])
     indep_test=pd.read_csv("D:\\bro\\Analysis\\mSANN\\cd\\yT.dat",sep='\s+',names=["target"])
 
-    best_params,best_model=tune_model(train_x=dep_train,train_y=indep_train,val_x=dep_val,val_y=indep_val,epochs=5,batch_size=32,project_name="soblenetwork_tuning")
-    with io.open('params.yaml', 'w', encoding='utf8') as outfile:
+    best_params=tune_model(train_x=dep_train,train_y=indep_train,val_x=dep_val,val_y=indep_val,epochs=5,batch_size=32,project_name="soblenetwork_tuning1")
+    with io.open('params1.yaml', 'w', encoding='utf8') as outfile:
         yaml.dump(best_params[0].values, outfile, default_flow_style=False, allow_unicode=True)
     print("file_name_ids_inside form:",best_params[0].values,file=sys.stderr)
--- newArchi/train.py
+++ newArchi/train.py
@@ -1,1 +1,47 @@
 ## uisng the YAML file generated we need to build the model using sequential()
+
+import tensorflow as tf
+import numpy as np
+import sklearn
+import random,os,io,pandas as pd, yaml,sys
+from tensorflow import keras
+from tensorflow.keras import layers
+import yaml
+from tensorflow.keras.models import Model,Sequential
+from tensorflow.keras.layers import Layer, Dense
+from tensorflow.keras.callbacks import Callback, EarlyStopping, LearningRateScheduler, ModelCheckpoint
+
+
+def create_model(params):
+    model = keras.Sequential()
+    model.add(layers.Input(16))
+    num_of_layers=int(list(params.keys())[-2][-1])
+    print(num_of_layers)
+    for i in range(num_of_layers):
+        print(i)
+        model.add(layers.Dense(units=params[f'units_{str(i+1)}'],kernel_initializer=params['weights'],
+                               activation=params[f'activation_{str(i+1)}']))
+    model.add(layers.Dense(1, activation='linear'))
+    model.compile(
+        optimizer=keras.optimizers.Adam(
+            params['learning_rate']),
+        loss='mean_absolute_error',
+        metrics=["mean_squared_error","mean_absolute_error","mean_absolute_percentage_error","mean_squared_logarithmic_error"])
+    return model
+
+if __name__=="__main__":
+    dep_train=pd.read_csv("D:\\bro\\Analysis\\mSANN\\cd\\XTr.dat",sep='\s+',names=[str(i) for i in range(0,16)])
+    indep_train=pd.read_csv("D:\\bro\\Analysis\\mSANN\\cd\\yTr.dat",sep='\s+',names=['target'])
+    dep_val=pd.read_csv("D:\\bro\\Analysis\\mSANN\\cd\\XV.dat",sep='\s+',names=[str(i) for i in range(0,16)])
+    indep_val=pd.read_csv("D:\\bro\\Analysis\\mSANN\\cd\\yV.dat",sep='\s+',names=["target"])
+
+    with open("params1.yaml", 'r') as stream:
+        params = yaml.safe_load(stream)
+    model=create_model(params=params)
+
+    history=model.fit(dep_train, indep_train, batch_size=32, epochs=3, validation_data=(dep_val, indep_val))
+    model.save("model")
+    pd.DataFrame(history.history).to_csv("metrices.csv",index=False)
+
+
+
